47cdd91e353cfc68c9a9ddf13a25c018d56f8a81
varungautam1411 <archervg@gmail.com>
2025-04-09 16:46:59 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+APP_HOME="/home/ec2-user/app"
+JAR_FILE="$APP_HOME/s3-memorydb-loader-1.0.jar"
+CONFIG_FILE="$APP_HOME/config/application.properties"
+LOG_DIR="$APP_HOME/logs"
+
+# Create log directory if it doesn't exist
+mkdir -p $LOG_DIR
+
+# Run the application
+java -jar $JAR_FILE \
+    --spring.config.location=file:$CONFIG_FILE \
+    >> $LOG_DIR/application.log 2>&1
