3a4a1e601a470c152bcfdca4cd2bc7cc0b833d34
varungautam1411 <archervg@gmail.com>
2025-04-09 16:46:12 -0700

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Build the application
+mvn clean package
+
+# Copy to EC2
+scp -i "ec2.pem" /Users/bsvag/Downloads/s3-memorydb-loader-1.0.jar \
+    ec2-user@ec2-3-148-255-133.us-east-2.compute.amazonaws.com:/home/ec2-user/app/
+
+# Copy configuration
+scp -i "ec2.pem" src/main/resources/application.properties \
+    ec2-user@ec2-3-148-255-133.us-east-2.compute.amazonaws.com:/home/ec2-user/app/config/
+
+# Copy run script
+scp -i "ec2.pem" scripts/run.sh \
+    ec2-user@ec2-3-148-255-133.us-east-2.compute.amazonaws.com:/home/ec2-user/app/
 
