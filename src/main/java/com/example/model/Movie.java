ae4375247bf864c590fba2e51ca9c330ab9085d4
varungautam1411 <archervg@gmail.com>
2025-04-10 08:05:39 -0700

    Update Movie.java

--- src/main/java/com/example/model/Movie.java
+++ src/main/java/com/example/model/Movie.java
@@ -1,21 +1,45 @@
 package com.example.model;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Movie {
-    private List<Integer> watchedBy;
+    private List<WatchedBy> watchedBy;
     private String title;
     private String movieId;
     private int yearOfRelease;
 
-    // Getters and Setters
-    public List<Integer> getWatchedBy() { return watchedBy; }
-    public void setWatchedBy(List<Integer> watchedBy) { this.watchedBy = watchedBy; }
+    public List<WatchedBy> getWatchedBy() { return watchedBy; }
+    public void setWatchedBy(List<WatchedBy> watchedBy) { this.watchedBy = watchedBy; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public String getMovieId() { return movieId; }
     public void setMovieId(String movieId) { this.movieId = movieId; }
     public int getYearOfRelease() { return yearOfRelease; }
     public void setYearOfRelease(int yearOfRelease) { this.yearOfRelease = yearOfRelease; }
-}
 
+    public static class WatchedBy {
+        @JsonProperty("customer-id")
+        private String customerId;
+        @JsonProperty("movie-id")
+        private String movieId;
+        private int rating;
+        private String date;
+
+        // Getters
+        @JsonProperty("customer-id")
+        public String getCustomerId() { return customerId; }
+        @JsonProperty("movie-id")
+        public String getMovieId() { return movieId; }
+        public int getRating() { return rating; }
+        public String getDate() { return date; }
+
+        // Setters
+        public void setCustomerId(String customerId) { this.customerId = customerId; }
+        public void setMovieId(String movieId) { this.movieId = movieId; }
+        public void setRating(int rating) { this.rating = rating; }
+        public void setDate(String date) { this.date = date; }
+    }
+}
